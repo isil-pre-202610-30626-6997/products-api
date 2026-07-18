e24d0eb67ff29db3e9850c733af200c7913abf76
jmayta1984 <jmayta@pucp.edu.pe>
2026-07-17 22:33:39 -0500

    feat: add production server configuration to OpenAPI documentation

--- src/main/java/pe/isil/demo/productsapi/ProductsApiApplication.java
+++ src/main/java/pe/isil/demo/productsapi/ProductsApiApplication.java
@@ -5,13 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
 		info = @Info(
 				title = "Products API",
 				version = "1.0",
 				description = "API documentation for managing products"
-		)
+		),
+		servers = {
+				@Server(
+						url = "https://products-api-6997-1015952539697.southamerica-east1.run.app/",
+						description = "Production server"
+				)
+		}
 )
 @SpringBootApplication
 public class ProductsApiApplication {
